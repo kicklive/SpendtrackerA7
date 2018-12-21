4ffd258a51be0782e97c43d4c802511f84e21e19
dpmac <dpmac@dp-mac-MacBook-Air.local>
2018-12-21 00:21:30 -0500

    12202018_2

--- client/src/app/login/login.component.ts
+++ client/src/app/login/login.component.ts
@@ -13,16 +13,18 @@ export class LoginComponent implements OnInit {
     email:'',
     password:''
     };
-  
+  public message='';
 
   constructor(private auth:AuthenticationService,private router:Router) { }
 
   login(){
     //debugger;
+    this.message='';
     this.auth.login(this.credentials).subscribe((res)=>{
       this.router.navigateByUrl('/profile');
     },(err)=>{debugger;
       console.log('this is the error==>'+err);
+      this.message="Username/password incorrect "+err;
     })
   }
 
