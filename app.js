3bd2ebe438b4f3bd254c34569c603670a1612f38
dpmac <dpmac@dp-mac-MacBook-Air.local>
2019-01-16 00:20:47 -0500

    01162019_1

--- app.js
+++ app.js
@@ -20,10 +20,10 @@ app.use(passport.initialize());
 console.log('in app.js');
 app.use('/', urlApi);
 app.use('/details', urlApi);
-//app.use('/api', routesApi);
+app.use('/api', routesApi);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
     var err = new Error('Not Found');
     err.status = 404;
     next(err);
@@ -32,7 +32,7 @@ app.use(function(req, res, next) {
 // error handlers
 
 // [SH] Catch unauthorised errors
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
     if (err.name === 'UnauthorizedError') {
         res.status(401);
         res.json({
@@ -44,7 +44,7 @@ app.use(function(err, req, res, next) {
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-    app.use(function(err, req, res, next) {
+    app.use(function (err, req, res, next) {
         res.status(err.status || 500);
         res.render('error', {
             message: err.message,
@@ -55,7 +55,7 @@ if (app.get('env') === 'development') {
 
 // production error handler
 // no stacktraces leaked to user
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
     res.status(err.status || 500);
     res.render('error', {
         message: err.message,
--- client/src/app/login/login.component.ts
+++ client/src/app/login/login.component.ts
@@ -12,27 +12,28 @@ import { NavstateService } from "../navstate.service";
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-  credentials:TokenPayload={
-    email:'',
-    password:''
-    };
-  public message='';
-  userDetails:UserDetails;
+  credentials: TokenPayload = {
+    email: '',
+    password: ''
+  };
+  public message = '';
+  userDetails: UserDetails;
 
-  constructor(private auth:AuthenticationService,private router:Router,private n:NavstateService) { 
-    
+  constructor(private auth: AuthenticationService, private router: Router, private n: NavstateService) {
+
   }
-  
-  login(){
-//debugger;
-    this.message='';
-    this.auth.login(this.credentials).subscribe((res)=>{
+
+  login() {
+    debugger;
+    this.message = '';
+    this.auth.login(this.credentials).subscribe((res) => {
+      debugger;
       this.n.setNavBarState(false);
-      this.n.setUserName('Hello, '+this.auth.getUsername());
-      this.router.navigateByUrl('/ST',{skipLocationChange:true});
-    },(err)=>{//debugger;
-      console.log('this is the error==>'+err);
-      this.message="There was a problem with the registration. Contact administrator. "+err;
+      this.n.setUserName('Hello, ' + this.auth.getUsername());
+      this.router.navigateByUrl('/ST', { skipLocationChange: true });
+    }, (err) => {//debugger;
+      console.log('this is the error==>' + err);
+      this.message = "There was a problem with the registration. Contact administrator. " + err;
     })
   }
 
