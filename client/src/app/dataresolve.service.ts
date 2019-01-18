b2317e5ce56879b3857aba75fb917630ecdb61f8
dpmac <dpmac@dp-mac-MacBook-Air.local>
2019-01-17 23:29:28 -0500

    01172019_2

--- client/src/app/dataresolve.service.ts
+++ client/src/app/dataresolve.service.ts
@@ -1,54 +1,42 @@
-import {Injectable }from '@angular/core'; 
-import {Router, Resolve, ActivatedRouteSnapshot }from "@angular/router"; 
-import {BudgetDetails }from "./budgetdata"; 
-import {BudgetDataService }from "./budget-data.service"; 
-import {SharedService }from "./shared.service"; 
-import {Observable, Subscription, of, BehaviorSubject }from 'rxjs'; 
-import {map }from 'rxjs/operators'; 
-import {mapTo, delay }from 'rxjs/operators'; 
-import {PersistanceService }from "./persistance.service"; 
+import { Injectable } from '@angular/core';
+import { Router, Resolve, ActivatedRouteSnapshot } from "@angular/router";
+import { BudgetDetails } from "./budgetdata";
+import { BudgetDataService } from "./budget-data.service";
+import { SharedService } from "./shared.service";
+import { Observable, Subscription, of, BehaviorSubject } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { mapTo, delay } from 'rxjs/operators';
+import { PersistanceService } from "./persistance.service";
 
 @Injectable()
-export class DataresolveService implements Resolve < BudgetDetails >  {
-// private serviceSubscription;
-//private id:string;
-
-// private bs=new BehaviorSubject<string>("");
-// currentMsg=this.bs.asObservable();
-val:string; 
+export class DataresolveService implements Resolve<BudgetDetails>  {
+	val: string;
 
 
 
-constructor(private bds:BudgetDataService, private router:Router, private service:SharedService, private ps:PersistanceService) {
-// this.serviceSubscription=this.service.emmiter.subscribe({
-//   next:(budget_id:string)=>{
-//     debugger;
-//     //this.GetDetails(budget_id);
-//     this.id=budget_id;
-//   }
-// });
-}
-resolve():Observable < any >  {
-debugger; 
-this.ps.currentMsg.subscribe((x) =>  {
-this.val = x; 
-}); 
-//return this.bds.GetBudgetDetails(this.bs.value).pipe(map((ret)=>{
-return this.bds.GetBudgetDetails(this.val).pipe(map((ret) =>  {
-debugger; 
-if (ret) {
-return (ret); 
-}
-else {
-this.router.navigate(['/']); 
-return false; 
-}
-})); 
+	constructor(private bds: BudgetDataService, private router: Router, private service: SharedService, private ps: PersistanceService) {
+	}
 
-}
+	resolve(): Observable<any> {
+		this.ps.currentMsg.subscribe((ret) => {
+			this.val = ret;
+		});
 
-// changeMsg(m:string){
+		return this.bds.GetBudgetDetails(this.val).pipe(map((ret) => {
+			debugger;
+			if (ret) {
+				return (ret);
+			}
+			else {
+				this.router.navigate(['/']);
+				return false;
+			}
+		}));
+
+	}
+
+	// changeMsg(m:string){
 	//  // debugger;
 	//   this.bs.next(m);
-  // }
+	// }
 }
